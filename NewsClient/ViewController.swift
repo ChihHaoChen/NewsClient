cfced1476ece8255d4c89a2597aba4cdc3385380
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2019-10-21 14:24:50 -0400

    Initial Commit

---
+++ NewsClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsClient
+//
+//  Created by chihhao on 2019-10-21.
+//  Copyright © 2019 ChihHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
