a84000d345dc2f554e5437980638ad3f623c043d
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2019-10-22 15:41:49 -0400

    UI adjustment of UICollectionVewCell in SearchNewsController.

--- NewsClient/View/Search/NewsSearchCell.swift
+++ NewsClient/View/Search/NewsSearchCell.swift
@@ -13,12 +13,12 @@ class NewsSearchCell:  UICollectionViewCell  {
         didSet  {
             nameLabel.text = articleResult.title
             imageView.sd_setImage(with: URL(string: articleResult.urlToImage ?? ""), completed: nil)
-            descriptionLabel.text = "\(articleResult.source?.name ?? "") • \(articleResult.publishedAt ?? "")"
+            descriptionLabel.text = "\(articleResult.source?.name ?? "") • \(articleResult.publishedAt?.prefix(10) ?? "")"
         }
     }
     let screenWidth = UIScreen.main.bounds.width
     let imageView = UIImageView(cornerRadius: 12)
-    let nameLabel = UILabel(text: "News Title", font: .systemFont(ofSize: 16*(UIScreen.main.bounds.width/320)), numberOfLines: 2)
+    let nameLabel = UILabel(text: "News Title", font: .systemFont(ofSize: 16*(UIScreen.main.bounds.width/320)), numberOfLines: 3)
     let descriptionLabel = UILabel(text: "Author Label", font: .systemFont(ofSize: 13))
     let separator: UIView = {
         let view = UIView()
@@ -30,8 +30,7 @@ class NewsSearchCell:  UICollectionViewCell  {
         super.init(frame: frame)
         
         imageView.image = #imageLiteral(resourceName: "News_iOS_Icon")
-//        imageView.constrainWidth(constant: 72)
-//        imageView.clipsToBounds = true
+        imageView.clipsToBounds = true
         imageView.constrainWidth(constant: self.frame.width/2.5 )
         imageView.constrainHeight(constant: self.frame.width/4)
         
@@ -41,10 +40,10 @@ class NewsSearchCell:  UICollectionViewCell  {
             ], customSpacing: 4)
         stackView.spacing = 12.8*(screenWidth/320)
         addSubview(stackView)
-        stackView.fillSuperview(padding: .init(top: 0, left: 6, bottom: 0, right: 6))
+        stackView.fillSuperview(padding: .init(top: 0, left: 16, bottom: 0, right: 16))
         stackView.alignment = .center
         addSubview(separator)
-        separator.anchor(top: nil, leading: nameLabel.leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: -4, right: 0), size: .init(width: 0, height: 0.5))
+        separator.anchor(top: nil, leading: nameLabel.leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: -8, right: 0), size: .init(width: 0, height: 0.5))
     }
     
     required init?(coder aDecoder: NSCoder) {
